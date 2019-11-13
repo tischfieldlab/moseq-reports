42525b2ed2f4a666c5c5abe32a21066557216ff3
jmc07712 <jmc07712@yahoo.com>
2019-11-13 14:39:48 -0500

    changing push step

--- travis-com.sh
+++ travis-com.sh
@@ -10,12 +10,14 @@ setup_git() {
 }
 
 commit_website_files() {
+  git checkout -b gh-pages
   git add .
   git commit --message "Travis build: $TRAVIS_BUILD_NUMBER"
 }
 
 upload_files() {
-  git push
+  git remote add origin-pages https://${GH_TOKEN}@github.com/tischfieldlab/vue-reports.git > /dev/null 2>&1
+  git push --quiet --set-upstream origin-pages gh-pages
 }
 
 setup_git
