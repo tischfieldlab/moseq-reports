39e9ea455890073c674730b4975f5b65c8fe4d00
jmc07712 <jmc07712@yahoo.com>
2019-11-13 14:59:01 -0500

    changing push step

--- travis-com.sh
+++ travis-com.sh
@@ -7,16 +7,19 @@ pwd
 setup_git() {
   git config --global user.email "travis@travis-ci.org"
   git config --global user.name "Travis CI"
+  git config --global push.default current
+  git stash
 }
 
 commit_website_files() {
   git checkout ${TRAVIS_BRANCH}
+  git stash pop
   git add .
   git commit --message "Travis build: $TRAVIS_BUILD_NUMBER"
 }
 
 upload_files() {
-  git push HEAD:${TRAVIS_BRANCH}
+  git push https://${GH_TOKEN}@github.com/tischfieldlab/vue-reports.git
 }
 
 setup_git
