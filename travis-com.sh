7e42def1f668e8122ed8e1038ff211461342bcee
jmc07712 <jmc07712@yahoo.com>
2019-11-13 15:38:18 -0500

    changing push step

--- travis-com.sh
+++ travis-com.sh
@@ -5,18 +5,19 @@ cd ..
 pwd
 
 setup_git() {
-  git config --global user.email "jmc07712@yahoo.com"
-  git config --global user.name "jmc07712"
+  git config --global user.email "travis@travis-ci.org"
+  git config --global user.name "Travis CI"
+  git config --global push.default matching
 }
 
 commit_website_files() {
   git checkout -b build-branch
-  git add . ./moseq-reports/package.json
+  git add . ./moseq-reports/*.json
   git commit --message "Travis build: $TRAVIS_BUILD_NUMBER"
 }
 
 upload_files() {
-  git remote add origin-build https://jmc07712@github.com/tischfieldlab/vue-reports.git > /dev/null 2>&1
+  git remote add origin-build https://${GH_TOKEN}@github.com/tischfieldlab/vue-reports.git > /dev/null 2>&1
   git push --quiet --set-upstream origin-build build-branch
 }
 
