cdf7a713fa87af2030616152ac1e462b6300f369
quantumdot <thackray@rutgers.edu>
2020-02-12 16:00:15 -0500

    more work on data loader

--- src/models/DataLoader.ts
+++ src/models/DataLoader.ts
@@ -1,5 +1,16 @@
 // import StreamZip from 'node-stream-zip';
 import DataModel, { MetadataJson } from './DataModel';
+const fs = require('fs');
+const path = require('path');
+const os = require('os');
+
+interface DataLoaderState {
+    bundle: string, // path to the bundle
+    path: string, // path to uncompressed data
+    datasets: Map<string, any>,
+}
+
+let currentState : DataLoaderState|undefined = undefined;
 
 
 export default function LoadDataBundle(filename: string) {
@@ -15,20 +26,43 @@ export default function LoadDataBundle(filename: string) {
         console.log('zip error', err);
     });
     zip.on('ready', () => {
+        currentState = {
+            bundle: filename,
+            path: fs.mkdtempSync(path.join(os.tmpdir(), 'moseq-reports-')),
+            datasets: new Map<string, any>(),
+        }
         // console.log('Entries read: ' + zip.entriesCount);
         // console.log('zip ready');
         LoadUsageData(zip);
+
+
+        //LoadCrowdMovies(zip); //for future use
 
         zip.close();
     });
 }
 
 function LoadSpinogramData(zip) {
+    if (currentState === undefined) {
+        throw new Error('unexpected current state is undefined!');
+    }
     const data = zip.entryDataSync('spinogram.corpus-sorted-usage.json');
+    currentState.datasets.set('spinogram', data);
 }
 
 function LoadUsageData(zip) {
     const data = zip.entryDataSync('metadata.json');
     const content: MetadataJson = JSON.parse(data) as MetadataJson;
     DataModel.loadMetadataFile(content);
+}
+
+function LoadCrowdMovies(zip) {
+    if (currentState === undefined) {
+        throw new Error('unexpected current state is undefined!');
+    }
+    const dest = path.join(currentState.path, 'crowd_movies');
+    fs.mkdirSync(dest);
+    zip.extract('crowd_movies', dest, (err, count) => {
+        console.log(err ? 'Extract error' : `Extracted ${count} entries`);
+    });
 }
