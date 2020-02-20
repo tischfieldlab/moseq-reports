5aeab8e117304d35400b17aa1edaf22894b9a17d
CBX1 <56642744+CBX1@users.noreply.github.com>
2020-02-20 11:45:31 -0500

    Update generate-data.py

--- generate-data.py
+++ generate-data.py
@@ -1,29 +1,52 @@
 import os, argparse, json
-
+import sys
+import glob
+import hashlib
+import os
+import re
+import subprocess
+import sys
 from moseq2_extras.util import ensure_dir
 from moseq2_viz.util import parse_index
 from moseq2_viz.model.util import parse_model_results, \
         results_to_dataframe
+import numpy as np
+import plotly.figure_factory as ff
+from scipy.cluster.hierarchy import linkage
+
+
+from moseq2_extras.util import NumpyEncoder
+from moseq2_viz.model.util import (get_transition_matrix, parse_model_results,
+                                   results_to_dataframe)
+from moseq2_viz.util import parse_index
 
 METADATAPATH = os.getcwd()
 
 def main():
+    index = input("Enter path of indexfile: ")
+    newindex = index.replace('\\','/')
+    modelfile = input("enter path of model file: ")
+    newmodel = modelfile.replace('\\','/')
+    outputpath = input('enter output path: ')
+    newoutput = outputpath.replace('\\','/')
     parser = argparse.ArgumentParser()
-    parser.add_argument('-i', help='Index file to be used to create groups.',
+    parser.add_argument('--../moseq2-index.yaml', help='Index file to be used to create groups.',
             dest='indexFile')
-    parser.add_argument('-m', help='Model file to be used to create figures.',
+    parser.add_argument('--../first_model.p', help='Model file to be used to create figures.',
             dest='modelFile')
-    parser.add_argument('--output', help='The output directory for the metadata.json file to be saved to.',
+    parser.add_argument('--../ok', help='The output directory for the metadata.json file to be saved to.',
             dest='outputPath')
     parser.add_argument('--filter', nargs='+', dest='filterGroups',
             help='The list of groups that are desired to be in the report.')
-    parser.set_defaults(indexFile=None, modelFile=None, outputPath=METADATAPATH, filterGroups=[])
+    parser.set_defaults(indexFile=newindex, modelFile=newmodel, outputPath=outputpath, filterGroups=[])
     args = parser.parse_args()
 
     df = convertModelToJson(args)
     dfGroups = getGroupsFromDataframe(df)
-
+    
     writeMetadataFile(df, dfGroups, args.outputPath)
+    create_crowd_movies('first_model.p','moseq2-index.yaml','ok', 100, False, 'usage')
+
 #end main()
 
 def writeMetadataFile(df, dfGroups, outputPath):
@@ -32,7 +55,8 @@ def writeMetadataFile(df, dfGroups, outputPath):
     outputPath = os.path.join(outputPath, 'metadata.js')
     with open(outputPath, 'w') as f:
         f.write('export let dataframeJson = {}\n'.format(df))
-        f.write('export let cohortGroups = {}'.format(dfGroups))
+        f.write('export let cohortGroups = {}\n'.format(dfGroups))
+
 #end writeMetadataFile
 
 def getGroupsFromDataframe(df):
@@ -63,6 +87,31 @@ def convertModelToJson(args):
 
   return dfJson
 #end createMetadataFile()
+def create_crowd_movies(model, index, out_dir, max_syl, sort, count):
+    out_dir = ensure_dir(out_dir)
+    
+    #check if movies already exist
+    
+    if ((sort and count == 'usage') or not sort) and len(glob.glob(os.path.join(out_dir, '*(usage)*.mp4'))) > 0:
+        sys.stderr.write("It appears crowd movies already exist. Skipping. \n")
+        return
+    elif (sort and count == 'frames') and len(glob.glob(os.path.join(out_dir, '*(frames)*.mp4'))) > 0:
+        sys.stderr.write("It appears crowd movies already exist. Skipping. \n")
+        return
+    
+    sys.stderr.write("Creating crowd movies at {}\n".format(out_dir))
+    args = [
+        'moseq2-viz',
+        'make-crowd-movies',
+        index,
+        model,
+        '--max-syllable', str(max_syl),
+        '--output-dir', out_dir,
+        '--sort', str(sort),
+        '--count', count
+    ]
+    subprocess.call(args)
+#end create_crowd_movies()
 
 if __name__ == '__main__':
     main()
