0d079062f0f66adb5b6d9dec8dc13f1a9a98e99c
Thackray <thackray@rutgers.edu>
2020-03-31 18:36:28 -0400

    add typedef

--- src/dataframe-js.d.ts
+++ src/dataframe-js.d.ts
@@ -6,3 +6,29 @@ declare module "*.json" {
     const value: any;
     export default value;
 }
+
+
+declare module 'vue/types/vue' {
+    interface Vue {
+        /**
+        * When you need to ensure the DOM has been updated Vue's nextTick 
+        * just doesn't work. You will need to use the double requestAnimationFrame 
+        * method. This is an elegant wrapper to allow you to use the double 
+        * requestAnimationFrame method within your Vue applications either 
+        * globally Vue.$forceNextTick(callback) or within a 
+        * method this.$forceNextTick(callback)
+        *
+        * Example:
+        * Vue.$forceNextTick(() => {
+        *      // Your code here.
+        * })
+        * 
+        * // or 
+        * 
+        * await Vue.$forceNextTick()
+        *
+        * @param callback function to call
+        */
+        $forceNextTick(callback?: Function): Promise<void>|void;
+    }
+}
