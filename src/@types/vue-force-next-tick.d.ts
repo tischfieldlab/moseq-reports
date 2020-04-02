70c74cb1d1028114b750de3dc9b17373aec79618
Thackray <thackray@rutgers.edu>
2020-04-02 15:17:28 -0400

    fix linting issues

--- src/@types/vue-force-next-tick.d.ts
+++ src/@types/vue-force-next-tick.d.ts
@@ -3,25 +3,25 @@ import Vue from 'vue';
 declare module 'vue/types/vue' {
     interface Vue {
         /**
-        * When you need to ensure the DOM has been updated Vue's nextTick 
-        * just doesn't work. You will need to use the double requestAnimationFrame 
-        * method. This is an elegant wrapper to allow you to use the double 
-        * requestAnimationFrame method within your Vue applications either 
-        * globally Vue.$forceNextTick(callback) or within a 
-        * method this.$forceNextTick(callback)
-        *
-        * Example:
-        * Vue.$forceNextTick(() => {
-        *      // Your code here.
-        * })
-        * 
-        * // or 
-        * 
-        * await Vue.$forceNextTick()
-        *
-        * @param callback function to call
-        */
-        $forceNextTick(callback: Function): void;
+         * When you need to ensure the DOM has been updated Vue's nextTick 
+         * just doesn't work. You will need to use the double requestAnimationFrame 
+         * method. This is an elegant wrapper to allow you to use the double 
+         * requestAnimationFrame method within your Vue applications either 
+         * globally Vue.$forceNextTick(callback) or within a 
+         * method this.$forceNextTick(callback)
+         *
+         * Example:
+         * Vue.$forceNextTick(() => {
+         *      // Your code here.
+         * })
+         * 
+         * // or 
+         * 
+         * await Vue.$forceNextTick()
+         *
+         * @param callback function to call
+         */
+        $forceNextTick(callback: () => void): void;
         $forceNextTick(): Promise<any>;
     }
 }
