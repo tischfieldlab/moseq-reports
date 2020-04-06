0c9f7c1b8e3dab822f54e66b65d7fb080f6c2ca0
Thackray <thackray@rutgers.edu>
2020-04-06 14:58:17 -0400

    implement canvas-based alternative

---
+++ src/components/data_components/TransitionsHeatmapCanvas/TransitionsHeatmapCanvas.vue
@@ -0,0 +1,240 @@
+<template>
+    <canvas ref="canvas" :width="outsideWidth" :height="outsideHeight">
+        
+        <!--<ColorScaleLegend :scale="scale.absz" :width="width / 2" :height="50" :transform="`translate(${outsideWidth / 2}, ${scale.o.bandwidth()+40})`" />-->
+    </canvas>
+</template>
+
+
+<script lang="ts">
+import Vue from 'vue';
+import RegisterDataComponent from '@/components/Core';
+import * as d3 from 'd3';
+import { GetScale } from '@/util/D3ColorProvider';
+import { scaleLinear, scaleBand, scaleOrdinal, scaleSequential } from 'd3-scale';
+import { range } from 'd3-array';
+import ColorScaleLegend from '@/components/Core/ColorScaleLegend.vue';
+import mixins from 'vue-typed-mixins';
+import LoadingMixin from '@/components/Core/LoadingMixin';
+import WindowMixin from '@/components/Core/WindowMixin';
+
+
+RegisterDataComponent({
+    friendly_name: 'Transitions Heatmap Canvas',
+    component_type: 'TransitionsHeatmapCanvas',
+    settings_type: 'TransitionsHeatmapOptions',
+    init_width: 400,
+    init_height: 500,
+    default_settings: {
+        abs_colormap: 'interpolateViridis',
+        rel_colormap: 'interpolatePuOr',
+    },
+});
+
+export default mixins(LoadingMixin, WindowMixin).extend({
+    components: {
+        ColorScaleLegend,
+    },
+    data() {
+        return {
+            timestamp: Date.now(),
+            abs_transitions: {},
+            rel_transitions: {},
+            selected_transition: '',
+            sub_padding: 5,
+            abs_vmin: Number.MAX_VALUE,
+            abs_vmax: Number.MIN_VALUE,
+            rel_vmin: Number.MAX_VALUE,
+            rel_vmax: Number.MIN_VALUE,
+            margin: {
+                top: 20,
+                right: 20,
+                bottom: 20,
+                left: 20,
+            },
+        };
+    },
+    computed: {
+        outsideWidth(): number {
+            return this.layout.width - 10;
+        },
+        outsideHeight(): number {
+            return this.layout.height - 41;
+        },
+        width(): number {
+            return this.outsideWidth - this.margin.left - this.margin.right;
+        },
+        height(): number {
+            return this.outsideHeight - this.margin.top - this.margin.bottom;
+        },
+        scale(): any {
+            const o = scaleBand()
+                    .domain(this.selectedGroups)
+                    .rangeRound([0, this.outsideWidth])
+                    .paddingInner(this.sub_padding / this.outsideWidth);
+            const x = scaleBand()
+                    .domain(range(100) as any[])
+                    .rangeRound([0, o.bandwidth()])
+                    .padding(0);
+            const y = scaleBand()
+                    .domain(range(100) as any[])
+                    .rangeRound([o.bandwidth(), 0])
+                    .padding(0);
+            const absz = scaleSequential(this.abs_colormap)
+                .domain([this.abs_vmin, this.abs_vmax]);
+            const relz = scaleSequential(this.rel_colormap)
+                .domain([this.rel_vmin, this.rel_vmax]);
+            const c = scaleLinear()
+                .domain(absz.domain())
+                .range([-this.width / 4, this.width / 4]);
+            return { x, y, absz, relz, o, c };
+        },
+        abs_colormap(): any {
+            return GetScale(this.settings.abs_colormap);
+        },
+        rel_colormap(): any {
+            return GetScale(this.settings.rel_colormap);
+        },
+        selectedGroups(): string[] {
+            return this.dataview.selectedGroups;
+        },
+        rootGroup(): string {
+            return this.settings.relative_diff_group;
+        },
+    },
+    mounted() {
+        this.prepare_data();
+        const ctx = (this.$refs.canvas as HTMLCanvasElement).getContext('2d');
+        if (ctx !== null) {
+            this.startDrawing(ctx);
+        }
+    },
+    methods: {
+        group_transitions(group: string): number[][] {
+            if (this.settings.show_relative_diff && group !== this.rootGroup) {
+                return this.rel_transitions[group];
+            }
+            return this.abs_transitions[group];
+        },
+        group_z(group: string) {
+            if (this.settings.show_relative_diff && group !== this.rootGroup) {
+                return this.scale.relz;
+            }
+            return this.scale.absz;
+        },
+        prepare_data() {
+            const absTrans = JSON.parse(JSON.stringify(this.$store.getters[`${this.datasource}/transitions`]));
+            const relTrans = JSON.parse(JSON.stringify(absTrans));
+            const root = this.selectedGroups[0];
+
+            let absVmin = Number.MAX_VALUE;
+            let absVmax = Number.MIN_VALUE;
+            let relVmin = Number.MAX_VALUE;
+            let relVmax = Number.MIN_VALUE;
+
+            for (const g of this.selectedGroups) {
+                for (let i = 0; i < absTrans[g].length; i++) {
+                    absVmin = Math.min(absVmin, ...absTrans[g][i]);
+                    absVmax = Math.max(absVmax, ...absTrans[g][i]);
+
+                    for (let j = 0; j < relTrans[g][i].length; j++) {
+                        relTrans[g][i][j] = relTrans[g][i][j] - relTrans[root][i][j];
+                    }
+                    relVmin = Math.min(relVmin, ...relTrans[g][i]);
+                    relVmax = Math.max(relVmax, ...relTrans[g][i]);
+                }
+            }
+            this.abs_vmin = absVmin;
+            this.abs_vmax = absVmax;
+            this.rel_vmin = -Math.max(Math.abs(relVmin), Math.abs(relVmax));
+            this.rel_vmax = Math.max(Math.abs(relVmin), Math.abs(relVmax));
+            this.abs_transitions = absTrans;
+            this.rel_transitions = relTrans;
+        },
+        highlight_transition(event: MouseEvent) {
+            if (event && event.srcElement && event.srcElement instanceof SVGRectElement) {
+                const t = event.srcElement.dataset.transition as string;
+                this.selected_transition = t;
+            }
+        },
+        startDrawing(context: CanvasRenderingContext2D) {
+            const draw = () => {
+                if (Date.now() < (this.timestamp + 900)) {
+                    return requestAnimationFrame(draw);
+                }
+
+                context.clearRect(0, 0, this.outsideWidth, window.outerHeight);
+
+                for (const group of this.selectedGroups) {
+                    const offset = this.scale.o(group);
+
+                    const labelSize = context.measureText(group);
+                    context.fillStyle = '#000000';
+                    context.fillText(group,
+                                     (offset + (this.scale.o.bandwidth() / 2) - (labelSize.width / 2)),
+                                     10);
+
+                    for (const [rid, rows] of this.group_transitions(group).entries()) {
+                        for (const [cid, value] of rows.entries()) {
+                            context.fillStyle = this.group_z(group)(value);
+                            context.fillRect(this.scale.x(cid) + offset,
+                                             this.scale.y(rid),
+                                             this.scale.x.bandwidth(),
+                                             this.scale.y.bandwidth());
+                        }
+                    }
+                }
+                this.timestamp = Date.now();
+                requestAnimationFrame(draw);
+            };
+            draw();
+        },
+    },
+    directives: {
+        axis(el, binding, vnode) {
+            // console.log('axis directive called', el, binding);
+            const axis = binding.arg;
+            if (axis !== undefined) {
+                const axisMethod = { x: 'axisBottom', y: 'axisRight', c: 'axisBottom' }[axis];
+                const methodArg = binding.value[axis];
+                const actualAxis = d3[axisMethod](methodArg);
+
+                // if colorbar axis, only show 5 ticks
+                if (axis === 'c') {
+                    actualAxis.ticks(5);
+                }
+
+                // build the axis
+                d3.select(el).call(actualAxis);
+            }
+        },
+    },
+});
+</script>
+
+
+<style scoped>
+svg >>> g.heatmap text.label,
+svg >>> g.legend text.label {
+    font-family: Verdana,Arial,sans-serif;
+    font-size: 13px;
+    text-anchor:middle;
+    fill:#000;
+}
+svg >>> g.legend g.tick line {
+    stroke: #888;
+}
+svg >>> g.legend g.tick text,
+svg >>> g.legend text.label {
+    fill: #888;
+}
+svg >>> g.legend path.domain {
+    stroke:none;
+}
+
+svg >>> rect.selected {
+    transform: matrix(3,0,0,3,0,0);
+    transform-origin: 50% 50%;
+    stroke: #000;
+}
+</style>
