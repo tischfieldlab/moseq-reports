d6888ec6c7cf008a46389fa1bfe222053b57a93d
Josh Thackray <thackray@rutgers.edu>
2020-05-28 14:40:54 -0400

    add filters for owner, arbitrary filters

--- src/components/DatasetPicker.vue
+++ src/components/DatasetPicker.vue
@@ -13,22 +13,32 @@ export default Vue.component('dataset-picker', {
         },
         dataview: {
             type: Object, // Dataview
+        },
+        filters: {
+            type: Function,
+            default: ((item) => true) as (item: any) => boolean,
+        },
+        owner: {
+            type: String,
         }
     },
     computed: {
         options(): {text: string, value: string}[] {
-            return Object.entries(this.dataview.views).map(([key, dset]) => {
+            return (Object.entries(this.dataview.views).map(([key, dset]) => {
                 const parts = key.split('/');
                 const win = this.$store.state.datawindows[parts[1]];
-                if (win !== undefined) {
+                if (win !== undefined && (this.owner !== undefined && this.owner !== parts[1])) {
                     return {
+                        window: win,
                         text: `${win.title}: ${parts[2]}`,
                         value: key,
                     };
                 } else {
                     return undefined;
                 }
-            }).filter((item) => item !== undefined) as {text: string, value: string}[];
+            })
+            .filter((item) => item !== undefined) as {window: any, text: string, value: string}[])
+            .filter(this.filters as (item: any) => boolean);
         },
     },
     methods: {
