94fdd1705aafbe45f820f61727e8845cf5810245
Josh Thackray <thackray@rutgers.edu>
2021-04-05 16:40:45 -0400

    allow sidebar to be placed on left or right

---
+++ src/commands/SidebarPosition.ts
@@ -0,0 +1,19 @@
+import store from '@/store/root.store';
+import {SidebarPosition} from '@/store/root.types';
+
+
+export function SetSidebarRight() {
+    store.commit('setSidebarPosition', SidebarPosition.Right);
+}
+export function SetSidebarLeft() {
+    store.commit('setSidebarPosition', SidebarPosition.Left);
+}
+export function CurrentSidebarPosition() {
+    return store.state.sidebarPosition;
+}
+export function isSidebarLeft() {
+    return store.state.sidebarPosition === SidebarPosition.Left;
+}
+export function isSidebarRight() {
+    return store.state.sidebarPosition === SidebarPosition.Right;
+}
