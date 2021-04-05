af7c1d3145b9d7d02a91ccf3bf5bfff48f824361
Josh Thackray <thackray@rutgers.edu>
2021-04-05 16:47:41 -0400

    fix merge

---
+++ src/components/data_components/MutationPlot/MutationPlot.vue
@@ -0,0 +1,140 @@
+<template>
+    <box-plot
+        :width="this.layout.width"
+        :height="this.layout.height - 31"
+        :data="individualUseageData"
+        :groupLabels="groupNames"
+        :groupColors="groupColors"
+        :whisker_type="settings.boxplot_whiskers"
+        :show_boxplot="settings.show_boxplot"
+        :show_points="settings.show_points"
+        :show_violinplot="settings.show_violinplot"
+        :point_size="settings.point_size"
+        xAxisTitle="Group"
+        :yAxisTitle="`Module #${selectedSyllable} Usage (${countMethod})`"
+        :tooltipFormatter="format_tooltip"
+    />
+</template>
+
+<script lang="ts">
+import Vue from 'vue';
+import RegisterDataComponent from '@/components/Core';
+import LoadingMixin from '@/components/Core/LoadingMixin';
+import mixins from 'vue-typed-mixins';
+import WindowMixin from '@/components/Core/WindowMixin';
+
+import BoxPlot from '@/components/Charts/BoxPlot/BoxPlotCanvas.vue';
+import {WhiskerType} from '@/components/Charts/BoxPlot/BoxPlot.types';
+import LoadData from '@/components/Core/DataLoader/DataLoader';
+import { CountMethod } from '@/store/dataview.types';
+import {OrderingType} from '@/components/Charts/ClusteredHeatmap/ClusterHeatmap.types';
+import { Operation } from '../../Core/DataLoader/DataLoader.types';
+
+
+
+RegisterDataComponent({
+    friendly_name: 'Usage Details',
+    component_type: 'DetailedUsage',
+    settings_type: 'DetailedUsageOptions',
+    init_width: 400,
+    init_height: 500,
+    default_settings: {
+        show_points: true,
+        point_size: 2,
+        show_boxplot: true,
+        show_violinplot: false,
+        boxplot_whiskers: WhiskerType.TUKEY,
+        group_order_type: OrderingType.Natural,
+    },
+});
+
+export default mixins(LoadingMixin, WindowMixin).extend({
+    components: {
+        BoxPlot,
+    },
+    data() {
+        return {
+            individualUseageData: [],
+        };
+    },
+    watch: {
+        dataset: {
+            handler(): any {
+                LoadData(this.dataset[0], this.dataset[1])
+                .then((data) => this.individualUseageData = data);
+            },
+            immediate: true,
+        },
+    },
+    computed: {
+        selectedSyllable(): number {
+            return this.dataview.selectedSyllable;
+        },
+        countMethod(): string {
+            return this.dataview.countMethod;
+        },
+        groupNames(): string[] {
+            if (this.settings.group_order_type === OrderingType.Natural) {
+                return this.dataview.selectedGroups;
+            } else if (this.settings.group_order_type === OrderingType.Dataset) {
+                if (this.dataview.views[this.settings.group_order_dataset] !== undefined) {
+                    return this.dataview.views[this.settings.group_order_dataset].data;
+                }
+            } else {
+                // tslint:disable-next-line:no-console
+                console.warn(`Unsupported group order type ${this.settings.group_order_type}`);
+            }
+            return [];
+        },
+        groupColors(): string[] {
+            return this.dataview.groupColors;
+        },
+        dataset(): [string, Operation[]] {
+            return [
+                this.$store.getters[`datasets/resolve`]('usage'),
+                [{
+                    type: 'map',
+                    columns: [
+                        [`usage_${this.countMethod.toLowerCase()}`, 'value'],
+                        ['group', 'group'],
+                        [`id_${this.countMethod.toLowerCase()}`, 'syllable'],
+                        ['uuid', 'id'],
+                    ],
+                },
+                {
+                    type: 'filter',
+                    filters: {
+                        syllable: [this.selectedSyllable],
+                        group: this.dataview.selectedGroups,
+                    },
+                },
+                {
+                    type: 'sort',
+                    columns: ['value'],
+                    direction: 'asc',
+                }],
+            ];
+        },
+    },
+    methods: {
+        format_tooltip(itm: any): string {
+            if (itm.hasOwnProperty('id')) {
+                return `ID: ${itm.id.split('-').pop()}<br />
+                        Usage: ${itm.value.toExponential(3)}`;
+            } else if(itm.hasOwnProperty('count')) {
+                return `Group: ${itm.group}<br />
+                        Count: ${itm.count.toString()}<br />
+                        Median: ${itm.q2.toExponential(3)}<br />`;
+            } else {
+                return JSON.stringify(itm, undefined, '\t');
+            }
+        },
+    },
+});
+</script>
+
+
+
+<style scoped>
+
+</style>
---
+++ src/components/data_components/MutationPlot/MutationPlotOptions.vue
@@ -0,0 +1,190 @@
+<template>
+    <b-container fluid>
+        <b-row>
+            <b-col>
+                <b-input-group prepend="Group Ordering">
+                    <b-form-select v-model="group_order_type" :options="group_order_options"></b-form-select>
+                </b-input-group>
+            </b-col>
+        </b-row>
+        <b-row v-show="group_order_type === 'dataset'">
+            <b-col cols="1"></b-col>
+            <b-col>
+                <b-input-group prepend="Dataset">
+                    <DatasetPicker v-model="group_order_dataset" :dataview="dataview" :owner="subid" />
+                </b-input-group>
+            </b-col>
+        </b-row>
+        <b-row>
+            <b-form-checkbox v-model="show_points" switch>
+                Show Individual Data Points
+            </b-form-checkbox>
+        </b-row>
+        <b-row v-show="show_points">
+            <b-col cols="1"></b-col>
+            <b-col>
+                <b-input-group prepend="Point Size">
+                    <b-form-input type="number" v-model="point_size" min="1" max="10" ></b-form-input>
+                </b-input-group>
+            </b-col>
+        </b-row>
+        <b-row>
+            <b-form-checkbox v-model="show_boxplot" switch>
+                Show Boxplot
+            </b-form-checkbox>
+        </b-row>
+        <b-row v-show="show_boxplot">
+            <b-col cols="1"></b-col>
+            <b-col>
+                <b-input-group prepend="Whiskers">
+                    <b-form-select v-model="boxplot_whiskers" :options="whisker_options"></b-form-select>
+                    <div class="figure-caption">{{ boxplot_whisker_description }}</div>
+                </b-input-group>
+            </b-col>
+        </b-row>
+        <b-row>
+            <b-form-checkbox v-model="show_violinplot" switch>
+                Show Violin Plot
+            </b-form-checkbox>
+        </b-row>
+    </b-container>
+</template>
+
+<script scoped lang="ts">
+import Vue from 'vue';
+import mixins from 'vue-typed-mixins';
+import WindowMixin from '@/components/Core/WindowMixin';
+import {WhiskerType} from '@/components/Charts/BoxPlot/BoxPlot.types';
+import {OrderingType} from '@/components/Charts/ClusteredHeatmap/ClusterHeatmap.types';
+import DatasetPicker from '@/components/DatasetPicker.vue';
+
+
+export default mixins(WindowMixin).extend({
+    components: {
+        DatasetPicker,
+    },
+    computed: {
+        group_order_type: {
+            get(): string {
+                return this.settings.group_order_type;
+            },
+            set(value: string) {
+                this.$store.commit(`${this.id}/updateComponentSettings`, {
+                    id: this.id,
+                    settings: {
+                        group_order_type: value,
+                    },
+                });
+            },
+        },
+        group_order_dataset: {
+            get(): string {
+                return this.settings.group_order_dataset;
+            },
+            set(value: string) {
+                this.$store.commit(`${this.id}/updateComponentSettings`, {
+                    id: this.id,
+                    settings: {
+                        group_order_dataset: value,
+                    },
+                });
+            },
+        },
+        show_points: {
+            get(): boolean {
+                return this.settings.show_points;
+            },
+            set(value: boolean) {
+                this.$store.commit(`${this.id}/updateComponentSettings`, {
+                    id: this.id,
+                    settings: {
+                        show_points: value,
+                    },
+                });
+            },
+        },
+        point_size: {
+            get(): boolean {
+                return this.settings.point_size;
+            },
+            set(value: boolean) {
+                this.$store.commit(`${this.id}/updateComponentSettings`, {
+                    id: this.id,
+                    settings: {
+                        point_size: value,
+                    },
+                });
+            },
+        },
+        show_boxplot: {
+            get(): boolean {
+                return this.settings.show_boxplot;
+            },
+            set(value: boolean) {
+                this.$store.commit(`${this.id}/updateComponentSettings`, {
+                    id: this.id,
+                    settings: {
+                        show_boxplot: value,
+                    },
+                });
+            },
+        },
+        boxplot_whiskers: {
+            get(): WhiskerType {
+                return this.settings.boxplot_whiskers;
+            },
+            set(value: WhiskerType) {
+                this.$store.commit(`${this.id}/updateComponentSettings`, {
+                    id: this.id,
+                    settings: {
+                        boxplot_whiskers: value,
+                    },
+                });
+            },
+        },
+        boxplot_whisker_description: {
+            get(): string {
+                return this.whisker_options.find((wo) => wo.value === this.boxplot_whiskers)!.description;
+            },
+        },
+        show_violinplot: {
+            get(): boolean {
+                return this.settings.show_violinplot;
+            },
+            set(value: boolean) {
+                this.$store.commit(`${this.id}/updateComponentSettings`, {
+                    id: this.id,
+                    settings: {
+                        show_violinplot: value,
+                    },
+                });
+            },
+        },
+    },
+    data() {
+        return {
+            whisker_options: [
+                {
+                    value: WhiskerType.TUKEY,
+                    text: 'Tukey',
+                    description: 'Whiskers extend up to 1.5 * IQR from 25th and 75th percentile',
+                }, {
+                    value: WhiskerType.MIN_MAX,
+                    text: 'Min/Max',
+                    description: 'Whiskers extend to min and max data points',
+                },
+            ],
+            group_order_options: [
+                { text: 'Filter Order', value: OrderingType.Natural },
+                { text: 'Dataset', value: OrderingType.Dataset },
+            ],
+        };
+    },
+});
+</script>
+
+<style lang="scss" scoped>
+.row {
+    margin:10px 0;
+}
+</style>
