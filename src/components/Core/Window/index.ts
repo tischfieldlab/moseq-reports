076a474d7dfc0aa79e5713d42254fd7e553e66e7
Jared Chester <jmc07712@yahoo.com>
2021-12-15 17:10:58 -0500

    Fixing lint error

--- src/components/Core/Window/index.ts
+++ src/components/Core/Window/index.ts
@@ -2,4 +2,4 @@ export { default as BaseWindow } from './BaseWindow.vue';
 export { default as Window } from './Window.vue';
 export { default as WindowContainer } from './WindowContainer.vue';
 export { default as WindowMixin } from './WindowMixin';
-export { default as WindowTitlebarButton } from './WindowTitlebarButton.vue'; 
+export { default as WindowTitlebarButton } from './WindowTitlebarButton.vue';
