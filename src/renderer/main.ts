7863f01ddeaf556b4ed932dd184a0b67b3b85663
Jared Chester <jmc07712@yahoo.com>
2022-07-17 12:14:46 -0400

    Adding bootstrap-vue

--- src/renderer/main.ts
+++ src/renderer/main.ts
@@ -3,6 +3,13 @@ import Vue from "vue";
 import App from "./App.vue";
 // import './samples/node-api'
 
+import { BootstrapVue, IconsPlugin } from "bootstrap-vue";
+import "bootstrap/dist/css/bootstrap.css";
+import "bootstrap-vue/dist/bootstrap-vue.css";
+
+Vue.use(BootstrapVue);
+Vue.use(IconsPlugin);
+
 const app = new Vue({
   render: (h) => h(App),
 });
