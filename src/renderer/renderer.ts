f375727f1f37e04251490250b1ff2956d7a339f7
Jared Chester <jmc07712@yahoo.com>
2022-07-17 12:59:36 -0400

    Adding vue 2

--- src/renderer/renderer.ts
+++ src/renderer/renderer.ts
@@ -3,7 +3,9 @@ import Vue from "vue";
 // @ts-ignore
 import App from "@render/App.vue";
 
+// @ts-ignore
 import VueResize from "vue-resize";
+import { VueForceNextTick } from "vue-force-next-tick";
 import { BootstrapVue, IconsPlugin } from "bootstrap-vue";
 import "bootstrap/dist/css/bootstrap.css";
 import "bootstrap-vue/dist/bootstrap-vue.css";
@@ -11,6 +13,7 @@ import "bootstrap-vue/dist/bootstrap-vue.css";
 Vue.use(BootstrapVue);
 Vue.use(IconsPlugin);
 Vue.use(VueResize);
+Vue.use(VueForceNextTick);
 
 const app = new Vue({
   render: (h) => h(App),
