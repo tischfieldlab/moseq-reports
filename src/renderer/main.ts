89dc73bc5f1e75aabd7c415321ddd7486f5d67cd
Jared Chester <jmc07712@yahoo.com>
2022-07-17 12:22:13 -0400

    Add vue resize

--- src/renderer/main.ts
+++ src/renderer/main.ts
@@ -4,9 +4,11 @@ import App from "./App.vue";
 import { BootstrapVue, IconsPlugin } from "bootstrap-vue";
 import "bootstrap/dist/css/bootstrap.css";
 import "bootstrap-vue/dist/bootstrap-vue.css";
+import VueResize from "vue-resize";
 
 Vue.use(BootstrapVue);
 Vue.use(IconsPlugin);
+Vue.use(VueResize);
 
 const app = new Vue({
   render: (h) => h(App),
