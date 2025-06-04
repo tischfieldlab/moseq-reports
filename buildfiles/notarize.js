439ea88a70a550d22558a0deb7d84bf61e39bf8b
Josh Thackray <thackray@rutgers.edu>
2025-06-04 16:12:08 -0400

    getting build to work

---
+++ buildfiles/notarize.js
@@ -0,0 +1,35 @@
+import fs from 'fs';
+import path from 'path';
+import {notarize} from '@electron/notarize';
+
+export default async function(params) {
+    if (process.platform !== 'darwin') {
+        console.log(`skipping notarizing becuase operating system ${process.platform} is not macOS`)
+        return;
+    }
+
+    console.log('afterSign hook triggered', params);
+
+    let appId = 'edu.rutgers.tischfieldlab.moseq-reports'
+
+    let appPath = path.join(params.appOutDir, `${params.packager.appInfo.productFilename}.app`);
+    if (!fs.existsSync(appPath)) {
+        console.log(`skipping notarizing ${appPath} because it does not exist...`);
+        return;
+    }
+
+    console.log(`Notarizing ${appId} found at ${appPath}`);
+
+    try {
+        await notarize({
+            appBundleId: appId,
+            appPath: appPath,
+            appleId: process.env.APPLE_ID,
+            appleIdPassword: process.env.APPLE_ID_PSWD_MOSEQ_REPORTS,
+        });
+    } catch (error) {
+        console.error(error);
+    }
+
+    console.log(`Done notarizing ${appId}`);
+};
